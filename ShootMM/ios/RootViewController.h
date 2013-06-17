796a15dfc265dc6481629470d71de4787d723af7
mac <mac@mactekiMac-Pro.local>
2013-06-17 16:22:53 +0800

    Initial Commit

---
+++ ShootMM/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ShootMMAppController.h
+//  ShootMM
+//
+//  Created by mac on 13-6-17.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
